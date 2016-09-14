a1f927372ab5f55a7fc882c792bbd59bf837dda6
Katarina Tuttle and Stephanie Gurung <katarina+stephanie@>
2016-09-14 13:26:39 -0700

    display uploaded pictures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   # important
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:image, :email, :password) }
+    # devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:image, :email, :password) }
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :avatar, :password])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:email, :avatar, :password, :current_password])
   end
 end
